c0720084fdfe5b97b8c55fbaecbd98db12af783f
Lizz Hale <lizzhale@github.com>
2017-08-23 12:04:01 -0700

    Satisfy requirement 5

--- lizzhale/string_calculator2.rb
+++ lizzhale/string_calculator2.rb
@@ -4,16 +4,21 @@
 def add(string)
   delimiter = /,|\n/
   if string[-1] == "\n"
-    raise "Invalid input: cannot end in newline"
+    raise "cannot end in newline"
   elsif match = string.match(/\A\/\/(.)(\s|\S)/)
     if match[2] != "\n"
-      raise "Invalid input: newline required between custom delimiter and delimited numbers"
+      raise "newline required between custom delimiter and delimited numbers"
     end
     delimiter = match[1]
     string = string[3..-1]
   end
   sub_strings = string.split(delimiter)
-  sub_strings.inject(0) { |sum, sub_string| sum + sub_string.to_i }
+  numbers = sub_strings.map(&:to_i)
+  bad_numbers = numbers.select { |number| number < 0 }
+  if bad_numbers.any?
+    raise "negatives not allowed: #{bad_numbers.join(", ")}"
+  end
+  numbers.inject(0) { |sum, number| sum + number }
 end
 
 def assert_equal(expected, actual)
@@ -39,6 +44,7 @@ assert_equal 3, add("1,2")
 assert_equal 6, add("1,2,3")
 assert_equal 45, add("9,8,7,6,5,4,3,2,1")
 assert_equal 6, add("1\n2,3")
-assert_raises("Invalid input: cannot end in newline") { add("1,\n") }
+assert_raises("cannot end in newline") { add("1,\n") }
 assert_equal 3, add("//;\n1;2")
-assert_raises("Invalid input: newline required between custom delimiter and delimited numbers") { add("//;1;2") }
+assert_raises("newline required between custom delimiter and delimited numbers") { add("//;1;2") }
+assert_raises("negatives not allowed: -1, -3") { add("1, -1, -3") }
