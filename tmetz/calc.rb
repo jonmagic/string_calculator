3a9ec8eb51e8292b988a0ca31e7e6d0b1a3ac06a
Tammy Metz <tmetz@github.com>
2021-12-07 19:52:24 +0000

    changes from pairing with @jonmagic

--- tmetz/calc.rb
+++ tmetz/calc.rb
@@ -1,34 +1,60 @@
 require 'pry'
 
-class StringCalculator
+class Error < StandardError
 
-    # 1.
-    # Create a simple String calculator with a method signature:
-    # ———————————————
-    # int Add(string numbers)
-    # ———————————————
-    # The method can take up to two numbers, separated by commas, and will return their sum. 
-    # for example “” or “1” or “1,2” as inputs.
-    # (for an empty string it will return 0) 
-    #
-    # 2.
-    # Allow the Add method to handle an unknown amount of numbers
-    #
-    # 3.
-    # Allow the Add method to handle new lines between numbers (instead of commas).
-    # the following input is ok: “1\n2,3” (will equal 6)
-    # the following input is NOT ok: “1,\n” (not need to prove it - just clarifying)
-
-    def self.Add(numbers)
-        
-        number_arr = numbers.split(/,|\n/)
-        total = 0
-        number_arr.each do |number|
-            total += number.to_i
-        end
-        return total  
-    end
 end
 
-puts(StringCalculator.Add("1\n2,8,22"))
-puts(StringCalculator.Add(""))
+def add(numbers)
+    if numbers.start_with?("//") 
+        optional, numbers = numbers.split("\n", 2)
+        delimiter = optional[2..-1]
+        regexp = Regexp.new(delimiter)
+    else
+        regexp = Regexp.new(",|\n")
+    end
+
+    number_arr = numbers.split(regexp)
+    total = 0
+    negatives = []
+    number_arr.each do |number|
+        int_num = number.to_i
+        
+        if int_num < 0
+            negatives << int_num
+        else
+            total += int_num
+        end
+    end
+    if negatives.any?
+        neg_str = negatives.join(" ")
+        raise Error.new("negatives not allowed " + neg_str)
+    end
+    return total  
+end
+
+
+def test(numbers, expected)
+    actual = begin
+        add(numbers)
+    rescue Error => error
+        error.message
+    end
+    
+    ans = actual == expected
+    puts("Test add(#{numbers.inspect}) == #{expected}: #{ans}. \n Our answer: #{actual}")
+end
+
+test("", 0)
+test("5", 5)
+test("1,2", 3)
+test("1,2,8,22", 33)
+test("1\n5", 6)
+test("1\n5,3,5\n6", 20)
+test("//;\n1;2", 3)
+test("//<=>\n3<=>2<=>11", 16)
+test("9,-1,2,-33", "negatives not allowed -1 -33")
+# puts(StringCalculator.Add("1\n2,8,22\n5"))
+# puts(StringCalculator.Add(""))
+# my_str = "1\n2,8,22"
+# puts(my_str[1])
+# puts(my_str[2])
